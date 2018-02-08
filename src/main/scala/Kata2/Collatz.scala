aee445b83194eefd9430c261710554cc3bf3bd7c
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-02-08 09:37:29 +0000

    amend so easier to read

--- src/main/scala/Kata2/Collatz.scala
+++ src/main/scala/Kata2/Collatz.scala
@@ -7,18 +7,20 @@
   object Collatz extends App{
 
 
-    def collatz(n: Int, c: Int = 0): Int = {
+    def collatz(n: Int, count : Int = 0): Int = {
+
       if (n == 1) {
-        c+ 1
+        count + 1
 
       } else {
-        if (n % 2 == 0) collatz(n / 2, c + 1)
+        if (n % 2 == 0) collatz(n / 2, count + 1)
 
-        else collatz(n * 3 + 1, c + 1)
+        else collatz(3 *n + 1,count+1)
+
       }
     }
 
-    val enteredNum = StdIn.readLine("Enter a number: ").toInt
-    println(collatz(enteredNum))
+    val num = StdIn.readLine("Enter a number: ").toInt
+    println(collatz(num))
 
   }
